e20ccc3d5a93130dba5629c8cc950b7f5e44ed45
Joel Hampton <joelhampton@gmail.com>
2015-05-05 16:43:31 +1000

    Setup MongoDB model

--- api/models/Property.js
+++ api/models/Property.js
@@ -6,9 +6,46 @@
 */
 
 module.exports = {
-
   attributes: {
+    description: { type: 'array' }, 
+    title: { type: 'string' }, 
+    specification: {
+      property_type: { type: 'string' }, 
+      bathroom_no: { type: 'integer' }, 
+      bedroom_no: { type: 'integer' }, 
+      garage_no: { type: 'integer' }, 
+      floorplan: { type: 'string' }
+    }, 
+    agency: {
+      agency_website: { type: 'string' }
+    }, 
+    agent: {
+      profile: { type: 'string' }, 
+      name: { type: 'string' }, 
+      title: { type: 'string' }, 
+      photo: { type: 'string' }, 
+      phone: { type: 'string' }, 
+      email: { type: 'string' }
+    }, 
+    photos: { type: 'array' }, 
+    address: {
+      suburb: { type: 'string' }, 
+      state: { type: 'string' }, 
+      postcode: { type: 'integer' }, 
+      line1: { type: 'string' }
+    }, 
+    date: { type: 'date' }, 
+    listing_type: { type: 'string' }, 
+    price: {
+      sale_price: { type: 'string' }
+    }, 
+    metadata: {
+      url: { type: 'string' }, 
+      crawl_date: { type: 'integer' }, 
+      internal_id: { type: 'string' }, 
+      crawler_name: { type: 'string' }
+    }
+  },
 
-  }
 };
 
